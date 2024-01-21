e5bb9a9b163954e4d1b789800f373e1829cb99b3
Luiz de Freitas <luizdefreitas10@gmail.com>
2024-01-20 21:41:05 -0300

    feat: modal css

--- src/app/components/CustomModal/CustomModal.tsx
+++ src/app/components/CustomModal/CustomModal.tsx
@@ -6,6 +6,8 @@ import {
   ModalBody,
   ModalFooter,
   Button,
+  Input,
+  Checkbox,
 } from "@nextui-org/react";
 
 interface CustomModalProps {
@@ -15,46 +17,62 @@ interface CustomModalProps {
 
 const CustomModal: React.FC<CustomModalProps> = ({ isOpen, onClose }) => {
   return (
-    <Modal isOpen={isOpen} onOpenChange={onClose}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                Modal Title
-              </ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-  )
-}
+    <Modal isOpen={isOpen} onOpenChange={onClose} size="xl">
+      <ModalContent className="bg-[#222222] p-12">
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              <p className="text-[#C89A3D] text-[16px] font-montserratLight">
+                VAGÃO DE OURO
+              </p>
+              <h1 className="text-[24px] font-montserrat text-white font-bold">
+                COMO PARTICIPAR
+              </h1>
+            </ModalHeader>
+            <ModalBody>
+              <p className="font-montserratLight text-[16px] text-white font-normal">
+                Para participar, é bem fácil! Você deve ter um cadastro na
+                Esportes da Sorte e ter feito, pelo menos, um depósito de
+                R$2,00! Quanto mais depósitos você fizer, mais chances tem de
+                ganhar!
+              </p>
+              <p className="text-[#C89A3D] text-[16px] font-montserrat">
+                Responda as perguntas abaixo para confirmar sua participação.
+              </p>
+              <h1 className="text-[18px] font-montserrat text-white">
+                Você já é cadastrado na Esportes da Sorte?
+              </h1>
+              <div className="flex flex-col space-y-1 text-white">
+                <Checkbox
+                  radius="full"
+                  className="font-montserratLight text-white text-[13px]"
+                  classNames={{
+                    label: "text-white",
+                  }}
+                >
+                  SIM
+                </Checkbox>
+                <Checkbox
+                  radius="full"
+                  className="font-montserratLight text-white text-[13px]"
+                  classNames={{
+                    label: "text-white",
+                  }}
+                >
+                  NÃO
+                </Checkbox>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button size="sm" className="w-full bg-[#C89A3D] text-[#222222] py-3 px-8 font-headingBold text-[16px]">
+                AVANÇAR
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
 
-export default CustomModal
+export default CustomModal;
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,7 @@ module.exports = {
         headingExtraBold: ['Heading Pro Trial Extra Bold', 'sans-serif'],
         headingHeavy: ['Heading Pro Trial Heavy', 'sans-serif'],
         montserrat: ['Montserrat', 'sans-serif'],
+        montserratLight: ['Montserrat Light', 'sans-serif'],
         robotoRegular: ['Roboto Regular', 'sans-serif'],
         robotoBold: ['Roboto Bold', 'sans-serif'],
       },
